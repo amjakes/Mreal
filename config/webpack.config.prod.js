5959bd02bc2c832f5447815b53ed02b25cea4ae7
amjakes <murilajakes0@gmail.com>
2026-09-03 17:57:48 +0300

    update: meaningful update

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -10,6 +10,15 @@ var getClientEnvironment = require('./env');
 
 
 
+function ensureSlash(path, needsSlash) {
+  var hasSlash = path.endsWith('/');
+  if (hasSlash && !needsSlash) {
+    return path.substr(path, path.length - 1);
+  } else if (!hasSlash && needsSlash) {
+    return path + '/';
+  } else {
+    return path;
+  }
 }
 
 // We use "homepage" field to infer "public path" at which the app is served.
