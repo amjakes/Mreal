e5cb2c55d2e211205a753c0b9b8e336845dc1023
amjakes <murilajakes0@gmail.com>
2026-09-03 18:02:00 +0300

    update: meaningful update

--- scripts/start.js
+++ scripts/start.js
@@ -48,13 +48,7 @@ if (isSmokeTest) {
     }
   };
 }
-
-function setupCompiler(host, port, protocol) {
-  // "Compiler" is a low-level interface to Webpack.
-  // It lets us listen to some events and provide our own custom messages.
-  compiler = webpack(config, handleCompile);
-
-  // "invalid" event fires when you have changed a file, and Webpack is
+ have changed a file, and Webpack is
   // recompiling a bundle. WebpackDevServer takes care to pause serving the
   // bundle, so if you refresh, it'll wait instead of serving the old one.
   // "invalid" is short for "bundle invalidated", it doesn't imply any errors.
