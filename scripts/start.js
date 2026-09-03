26a52d67970b1b5b4c22adc180ec44aae7e05dba
amjakes <murilajakes0@gmail.com>
2026-09-03 18:04:26 +0300

    update: meaningful update

--- scripts/start.js
+++ scripts/start.js
@@ -133,7 +133,13 @@ function onProxyError(proxy) {
       chalk.red('Proxy error:') + ' Could not proxy request ' + chalk.cyan(req.url) +
       ' from ' + chalk.cyan(host) + ' to ' + chalk.cyan(proxy) + '.'
     );
-    console.lly send the proper error response to the client.
+    console.log(
+      'See https://nodejs.org/api/errors.html#errors_common_system_errors for more information (' +
+      chalk.cyan(err.code) + ').'
+    );
+    console.log();
+
+    // And immediately send the proper error response to the client.
     // Otherwise, the request will eventually timeout with ERR_EMPTY_RESPONSE on the client side.
     if (res.writeHead && !res.headersSent) {
         res.writeHead(500);
