27502c8b0c21994ffc4ff567c9c713703445356e
amjakes <murilajakes0@gmail.com>
2026-09-03 18:00:58 +0300

    update: meaningful update

--- scripts/start.js
+++ scripts/start.js
@@ -5,7 +5,12 @@ process.env.NODE_ENV = 'development';
 // that have already been set.
 // https://github.com/motdotla/dotenv
 require('dotenv').config({silent: true});
-e('http-proxy-middleware');
+
+var chalk = require('chalk');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var historyApiFallback = require('connect-history-api-fallback');
+var httpProxyMiddleware = require('http-proxy-middleware');
 var detect = require('detect-port');
 var clearConsole = require('react-dev-utils/clearConsole');
 var checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
