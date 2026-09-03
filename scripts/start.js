bc9c4e8e1a05c49b7907256d33963fe3b84865e0
amjakes <murilajakes0@gmail.com>
2026-09-03 18:07:02 +0300

    update: meaningful update

--- scripts/start.js
+++ scripts/start.js
@@ -207,11 +207,10 @@ function addMiddleware(devServer) {
 
     // Listen for the websocket 'upgrade' event and upgrade the connection.
     // If this is not done, httpProxyMiddleware will not try to upgrade until
+    // an initial plain HTTP request is made.
+    devServer.listeningApp.on('upgrade', hpm.upgrade);
   }
-
-  // Finally, by now we have certainly resolved the URL.
-  // It may be /index.html, so let the dev server try serving it again.
-  devServer.use(devServer.middleware);
+dleware);
 }
 
 function runDevServer(host, port, protocol) {
