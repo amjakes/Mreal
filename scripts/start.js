b59503147316cb5b58117741911230ec3141c26c
amjakes <murilajakes0@gmail.com>
2026-09-03 18:08:44 +0300

    update: meaningful update

--- scripts/start.js
+++ scripts/start.js
@@ -294,7 +294,15 @@ detect(DEFAULT_PORT).then(port => {
   if (port === DEFAULT_PORT) {
     run(port);
     return;
-  }to run the app on another port instead?';
+  }
+
+  if (isInteractive) {
+    clearConsole();
+    var existingProcess = getProcessForPort(DEFAULT_PORT);
+    var question =
+      chalk.yellow('Something is already running on port ' + DEFAULT_PORT + '.' +
+        ((existingProcess) ? ' Probably:\n  ' + existingProcess : '')) +
+        '\n\nWould you like to run the app on another port instead?';
 
     prompt(question, true).then(shouldChangePort => {
       if (shouldChangePort) {
