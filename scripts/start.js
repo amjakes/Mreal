222b7647c5213ae681c63d45a0ade0b88ef4c41a
amjakes <murilajakes0@gmail.com>
2026-09-03 18:02:53 +0300

    update: meaningful update

--- scripts/start.js
+++ scripts/start.js
@@ -78,7 +78,12 @@ function setupCompiler(host, port, protocol) {
     // options so we are going to "massage" the warnings and errors and present
     // them in a readable focused way.
     var messages = formatWebpackMessages(stats.toJson({}, true));
-    var 
+    var isSuccessful = !messages.errors.length && !messages.warnings.length;
+    var showInstructions = isSuccessful && (isInteractive || isFirstCompile);
+
+    if (isSuccessful) {
+      console.log(chalk.green('Compiled successfully!'));
+    }
 
     if (showInstructions) {
       console.log();
