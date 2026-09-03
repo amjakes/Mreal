5969592db03957a8b330d055878413e3e4284519
amjakes <murilajakes0@gmail.com>
2026-09-03 18:04:54 +0300

    update: meaningful update

--- scripts/start.js
+++ scripts/start.js
@@ -149,7 +149,11 @@ function onProxyError(proxy) {
     );
   }
 }
- = require(paths.appPackageJson).proxy;
+
+function addMiddleware(devServer) {
+  // `proxy` lets you to specify a fallback server during development.
+  // Every unrecognized request will be forwarded to it.
+  var proxy = require(paths.appPackageJson).proxy;
   devServer.use(historyApiFallback({
     // Paths with dots should still use the history fallback.
     // See https://github.com/facebookincubator/create-react-app/issues/387.
