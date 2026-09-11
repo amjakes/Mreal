e322fd38806febe627f70ab796e19febd502d6f2
amjakes <murilajakes0@gmail.com>
2026-09-11 23:16:25 +0300

    error

--- src/lib/contracts.js
+++ src/lib/contracts.js
@@ -6,13 +6,7 @@ export async function loadDeployment() {
   const deployment = await response.json();
   if (!deployment.eventTicketing?.address) {
     throw new Error('Contract configuration is empty. Run npm run deploy:local first.');
-  }
-  return deployment;
-}
-
-export async function connectWallet(deployment) {
-  if (!window.ethereum) throw new Error('Install a wallet such as MetaMask to use this application.');
-  const provider = new BrowserProvider(window.ethereum);
+  } BrowserProvider(window.ethereum);
   await provider.send('eth_requestAccounts', []);
   const network = await provider.getNetwork();
   if (Number(network.chainId) !== deployment.chainId) {
