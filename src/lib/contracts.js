0627fd53494051837778f904dba7c55ec2ec7914
amjakes <murilajakes0@gmail.com>
2026-09-11 23:18:56 +0300

    fix

--- src/lib/contracts.js
+++ src/lib/contracts.js
@@ -13,7 +13,9 @@ export async function loadDeployment() {
 export async function connectWallet(deployment) {
   if (!window.ethereum) throw new Error('Install a wallet such as MetaMask to use this application.');
   const provider = new BrowserProvider(window.ethereum);
-  await provider.se deployment.chainId) {
+  await provider.send('eth_requestAccounts', []);
+  const network = await provider.getNetwork();
+  if (Number(network.chainId) !== deployment.chainId) {
     throw new Error(`Switch your wallet to chain ${deployment.chainId} and reconnect.`);
   }
   const signer = await provider.getSigner();
